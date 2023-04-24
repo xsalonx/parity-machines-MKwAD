48ad651ca89fd5af2f494763d4bec4fdb43f9d2a
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2023-04-24 15:25:43 +0200

    Finish permutation parity machine implementation

--- client/example.py
+++ client/example.py
@@ -2,16 +2,27 @@ from parity_machines import PermutationMachine
 
 
 if __name__ == '__main__':
-    ppm1 = PermutationMachine(k=2, n=3)
-    ppm2 = PermutationMachine(k=2, n=3)
+    k, n, g = 2, 30, 64
+
+    ppm1 = PermutationMachine(k, n, g)
+    ppm2 = PermutationMachine(k, n, g)
+    eve = PermutationMachine(k, n, g)
+
+    count = 0
 
     while not (ppm1.synchronized() and ppm2.synchronized()):
-        x = ppm1.generate_input()
+        x, pi = ppm1.generate_input()
 
-        tau1, tau2 = ppm1(x), ppm2(x)
+        tau1, tau2, tauEve = ppm1(x, pi), ppm2(x, pi), eve(x, pi)
         ppm1.update(tau2)
         ppm2.update(tau1)
 
-    print(ppm1.get_key().hex())
-    print(ppm2.get_key().hex())
-    print()
+        if tau1 == tau2 == tauEve:
+            eve.update(tau1)
+
+        count += 1
+
+    print(f'Keys synchronized after {count} iterations:')
+    print(f'PPM1: {ppm1.get_key().hex()}')
+    print(f'PPM2: {ppm2.get_key().hex()}')
+    print(f'Eve:  {eve.get_key().hex()}')
--- parity_machines/permutation_machine.py
+++ parity_machines/permutation_machine.py
@@ -27,60 +27,78 @@ class PermutationMachine(ParityMachine):
         assert self.g >= k * n, "State vector size must be greater than or equal to the number of weights"
 
         self.s = np.random.randint(0, 2, self.g)
-        self.s_idx = 0
-        self.s_filled = False
+        self.buffer = np.empty(self.g, dtype=np.int8)
+
+        self.buffer_idx = 0
+        self.buffer_filled = False
         self.counter = 0
 
-    def __call__(self, x):
+    def __call__(self, x, pi):
         """
         The output of the hidden layer is the sum of XOR products of
-        the input and the weights. The output of the hidden layer is
+        the input and the weights. Weight vector is created by
+        permuting the state vector. The output of the hidden layer is
         then fed into a threshold function which outputs 1 if the sum
         is greater than `n / 2` and 0 otherwise. The output of the
         network is the product of the outputs of the hidden layers.
 
-        :param x:
+        :param x: Input vector
+        :param pi: Permutation vector
         :return: Output of the parity machine
         """
 
-        W = np.random.permutation(self.s).reshape(self.k, self.n)
+        W = self.s[pi].reshape(self.k, self.n)
         X = x.reshape(self.k, self.n)
 
         h = np.bitwise_xor(X, W).sum(axis=1)
         sigma = np.where(h > self.n / 2, 1, 0)
-        tau = np.logical_xor.reduce(sigma)
+        tau = np.bitwise_xor.reduce(sigma)
 
+        self.sigma0 = sigma[0]
         self.tau = tau
+
         return tau
 
     def update(self, tau):
         """
+        Performs the update of the parity machine. The update is
+        performed only if the output of the networks are equal.
+        The update consists of setting the current output of the
+        first hidden neuron to the buffer and incrementing the
+        buffer index. If the index is greater than or equal to the
+        buffer size, the state vector is filled with the contents
+        of the buffer and the buffer index is reset to 0.
 
         :param tau:
         :return:
         """
 
         if tau == self.tau:
-            self.s_idx += 1
+            self.buffer[self.buffer_idx] = self.sigma0
+
+            self.buffer_idx += 1
             self.counter += 1
 
-            if self.s_idx == self.g and not self.s_filled:
-                self.s_idx = 0
-                self.s_filled = True
-
-            # TODO learning
+            if self.buffer_idx == self.g:
+                self.buffer_filled = True
+                self.s = np.copy(self.buffer)
+                self.buffer_idx = 0
         else:
             self.counter = 0
 
     def generate_input(self):
         """
-        Generates a random input vector which consists of `k * n`
-        values which are either 0 or 1.
+        Generates a random input vector and a random permutation
+        vector  which consists of `k * n` values which are either
+        0 or 1.
 
-        :return: Random input vector
+        :return: Random input vector and permutation vector
         """
 
-        return np.random.randint(0, 2, self.k * self.n)
+        x = np.random.randint(0, 2, self.k * self.n)
+        pi = np.random.permutation(self.g)[:self.k * self.n]
+
+        return x, pi
 
     def get_key(self):
         """
@@ -100,8 +118,11 @@ class PermutationMachine(ParityMachine):
     def synchronized(self):
         """
         Returns whether the two parity machines are synchronized.
+        Synchronization is achieved when the buffer is filled and
+        the counter is greater than or equal to the buffer size which
+        means that the last `g` outputs of both networks were equal.
 
         :return: True if the two parity machines are synchronized, False otherwise
         """
 
-        return self.counter >= self.g
+        return self.buffer_filled and self.counter >= self.g
